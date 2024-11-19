f5557b898abe13c6f11c82f938c28ae539fa67de
NamTruong2003 <truongnamvxtd91@gmail.com>
2024-11-19 15:22:26 +0700

    server v0.8

---
+++ LidgrenServer/LidgrenServer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LidgrenServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Server server = new Server();
+        }
+    }
+}
